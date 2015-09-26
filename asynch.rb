25d0407eb9126c118046f8d91daa5fdfee6c331a
Christopher Huang <CCHuang90@gmail.com>
2015-09-26 16:28:59 -0700

    repeat interval

--- asynch.rb
+++ asynch.rb
@@ -14,11 +14,10 @@ class Asynch
 
   def run_loop
     @threads << Thread.new do
-      @queue = []
       while @run
-        to_run = @queue.shift
-        if to_run
-          # puts "running method #{to_run[:method]}"
+        unless @queue.empty?
+          to_run = @queue.shift
+          puts "running method #{to_run[:method]}"
           to_run_object= to_run[:object] || Object
           to_run_object.send(to_run[:method], *to_run[:args])
         end
@@ -30,31 +29,35 @@ class Asynch
     @run = false
   end
 
-  def queue_method(method, object = nil, args = [])
-    method_def = {}
-    method_def[:object] = object
-    method_def[:method] = method
-    method_def[:args] = args
-    @queue << method_def
+  def queue_method(method)
+    @queue << method
   end
 
-  def set_timeout(wait, callback, object = nil, args = [])
+  def set_timeout(wait, callback)
     @threads << Thread.new do
       sleep(wait)
-      self.queue_method(callback, object, args)
+      self.queue_method(callback)
     end
   end
 
-  def question(prompt, callback, object = nil, args = [])
+  def interval(wait_length, num_repeats = -1, callback)
     @threads << Thread.new do
-      puts "callback: #{callback}"
-      puts "object = #{object}"
-      puts "args = #{args}"
+      until num_repeats.zero?
+        puts "waiting #{wait_length} secs"
+        sleep(wait_length)
+        self.queue_method(callback)
+        num_repeats -= 1 unless num_repeats == -1
+      end
+    end
+  end
+
+  def question(prompt, callback)
+    @threads << Thread.new do
       puts prompt
       answer = gets.chomp
       args.unshift(answer)
       puts args
-      self.queue_method(callback, object, args)
+      self.queue_method(callback)
     end
   end
 
@@ -64,9 +67,11 @@ class Asynch
 
   def close_loop
     puts "will finish whats left to do, then end"
-    while @run
-      end_run if threads.select { |thread| thread.alive? }.length == 2
+    while true
+      break if threads.select { |thread| thread.alive? }.length == 1
     end
+    p "stopping the runloop now"
+    end_run
     @queue.each do |method|
       to_run_object= method[:object] || Object
       to_run_object.send(method[:method], method[:args])
@@ -100,13 +105,15 @@ end
 
 a = Asynch.new
 p Time.now
-# a.queue_method(:sleep, nil, 1)
-# a.queue_method(:my_puts,nil, "msg 1: run a method after a 1sec wait")
-# a.set_timeout(5, :my_puts, nil, "msg 3: run 5 secs after msg 2")
-# a.queue_method(:puts,nil,"msg 2: run immediately after msg 1")
-a.queue_method(:queue_method, a, [:my_puts, nil, "recursive call"] )
-a.question("Guess a number", :my_puts)
-a.queue_method(:close_loop, a)
+# a.queue_method(method: :sleep,args: 1)
+a.queue_method(method: :my_puts, args: "msg 1: run a method after a 1sec wait")
+# a.set_timeout(5,method: :my_puts,args: "msg 3: run 5 secs after msg 2")
+# a.queue_method(method: :puts, args: "msg 2: run immediately after msg 1")
+# a.queue_method(method: :queue_method, object: a, args: {method: :my_puts, args: "recursive call"} )
+# a.question("Guess a number", method: :my_puts)
+a.interval(1, 5, method: :my_puts,args: "test")
+# a.queue_method(method: :close_loop,object: a)
+a.set_timeout(2, method: :quit, object: a)
 puts "this should print first"
 ThreadsWait.all_waits(*a.threads)
 p Time.now
